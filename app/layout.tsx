856a75f4017ecace8e3e38c840da290f530cf8da
Konark Sharma <sharmakonark6k1@gmail.com>
2024-07-27 18:11:21 +0530

    [Fix]: Layout Changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,14 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
-
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
-const IBMPlex = IBM_Plex_Sans({
+const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-ibm-plex",
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-ibm-plex'
 });
 
 export const metadata: Metadata = {
@@ -29,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/" appearance={{
-      variables: {colorPrimary: '#624cf5'}
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
     }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
